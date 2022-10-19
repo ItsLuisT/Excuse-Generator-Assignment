6d39928acaaa97da84723d6ea12ea80377443288
Luis <ltrelles39@gmail.com>
2022-10-19 20:27:23 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", function() {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log();
+};
+
+let generateExcuse = function() {
+  let who = ["The dog", "My little brother", "My pet turtle", "My bird"];
+
+  let action = ["ate", "peed", "crushed", "broke"];
+
+  let what = ["my homework", "the keys", "the car"];
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
